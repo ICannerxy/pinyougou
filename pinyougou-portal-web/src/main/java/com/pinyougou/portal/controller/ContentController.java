baa1caef0be942e2ca300d0804029f9893bbed86
ICannerxy <icanner@qq.com>
2018-12-28 20:49:45 +0800

    新增广告管理，商城首页，Redis缓存

---
+++ pinyougou-portal-web/src/main/java/com/pinyougou/portal/controller/ContentController.java
@@ -0,0 +1,24 @@
+package com.pinyougou.portal.controller;
+
+import java.util.List;
+
+import com.pinyougou.sellergoods.service.ContentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.pojo.TbContent;
+
+@RestController
+@RequestMapping("/content")
+public class ContentController {
+
+	@Reference
+	private ContentService contentService;
+	
+	@RequestMapping("/findByCategoryId")
+	public List<TbContent> findByCategoryId(Long categoryId) {
+		return contentService.findByCategoryId(categoryId);
+	}
+	
+}
